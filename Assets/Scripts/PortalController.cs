ce32dcfcbb4052340dfc8e9157b722500cdbfe3c
tylerpinney88 <56723285+tylerpinney88@users.noreply.github.com>
2019-10-28 12:15:59 +0000

    Portals
    portals added and mostly coded, teleport is quite working

---
+++ Assets/Scripts/PortalController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalController : MonoBehaviour
+{
+    public GameObject AltPortal;
+    public GameObject Player;
+
+    public void Awake()
+    {
+     
+    }
+    public void Start()
+    {
+       
+    }
+
+    public void Update() 
+    {
+        AltPortalAssignment();
+    }
+
+    public void AltPortalAssignment() 
+    {
+        if (AltPortal == null) 
+        {
+            if (this.gameObject.tag == "Portal1")
+            {
+                AltPortal = GameObject.FindGameObjectWithTag("Portal2");
+            }
+            else
+            {
+                AltPortal = GameObject.FindGameObjectWithTag("Portal1");
+            }
+        }
+    }
+
+    public void OnTriggerEnter(Collider col) 
+    {
+        if (col.gameObject.tag == "Player") 
+        {
+            Debug.Log("Player seen");
+            Player.transform.position = AltPortal.transform.position;
+        }
+    }
+
+}
